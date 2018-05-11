0fe9e9bc4b25733e2072d71f468d825da3fc293a
Nora Giczy <zynori@gmail.com>
2018-05-11 14:58:28 +0200

    add new tasks

---
+++ week-01/day-04/countFromTwo/countFromTwo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace countFromTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("give me a number: ");
+            int number01 = int.Parse(Console.ReadLine());
+
+            Console.Write("give me another number: ");
+            int number02 = int.Parse(Console.ReadLine());
+
+            if (number01 > number02)
+            {
+                Console.WriteLine("The second number should be bigger");
+            }
+            else if (number01 < number02)
+            {
+                for (int i = number01; i < number02; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-01/day-04/multiplicationTable/multiplicationTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("give me a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i + " * " + number + " = " + i * number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
