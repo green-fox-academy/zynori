647b9450d608d55989b2da3c784b8b3a0401ad5a
Nora Giczy <zynori@gmail.com>
2018-05-12 20:15:25 +0200

    add new tasks

---
+++ week-01/day-04/drawDiagonal/drawDiagonal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drawDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number: ");
+
+            int number = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (i == j || i == 0 || j == 0 || i == number -1 || j == number -1)
+                    {
+                        Console.Write("%");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-01/day-04/drawTriangle/drawTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number: ");
+
+            int number = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
