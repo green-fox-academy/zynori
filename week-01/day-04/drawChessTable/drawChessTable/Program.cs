41a16e063dc44c1cb0a164b724e4121068ea13b4
Nora Giczy <zynori@gmail.com>
2018-05-13 15:37:06 +0200

    complete new task

--- week-01/day-04/drawChessTable/drawChessTable/Program.cs
+++ week-01/day-04/drawChessTable/drawChessTable/Program.cs
@@ -10,6 +10,22 @@ namespace drawChessTable
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i <= 8; i++)
+            {
+                for (int j = 0; j <= 3; j++)
+                {
+                    if (i % 2 == 0 )
+                    {
+                        Console.Write("% ");
+                    }
+                    else
+                    {
+                        Console.Write(" %");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
