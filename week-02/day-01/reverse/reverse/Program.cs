63258c8d0c3337f5abe7f5bbde67d9d3f5aebc7d
Nora Giczy <zynori@gmail.com>
2018-05-20 16:11:14 +0200

    feat(Reverse): done

---
+++ week-02/day-01/reverse/reverse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aj = new int[] { 3, 4, 5, 6, 7 };
+            int[] temp = new int[aj.Length];
+            for (int i = 0; i < aj.Length; i++)
+            {
+                temp[i] = aj[aj.Length - i - 1];
+                Console.WriteLine(temp[i]);
+            }
+            Console.ReadLine();
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+        }
+    }
+}
---
+++ week-02/day-01/unique/unique/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace unique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Unique(new int[] { 1, 11, 34, 11, 52, 61, 1, 34 });
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.ReadLine();
+            //  Create a function that takes a list of numbers as a parameter
+            //  Returns a list of numbers where every number in the list occurs only once
+
+            //  Example
+            // Console.WriteLine(Unique({ 1, 11, 34, 11, 52, 61, 1, 34}));
+            //  should print: `[1, 11, 34, 52, 61]`
+        }
+
+        public static List<int> Unique(int[] array)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!list.Contains(array[i]))
+                {
+                    list.Add(array[i]);
+                }
+            }
+            return list;
+        }
+    }
+}
