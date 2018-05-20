89ec34dca0a7ecc03e1f4526b7bf3965e71d4bdf
Nora Giczy <zynori@gmail.com>
2018-05-20 16:43:31 +0200

    add new task

---
+++ week-02/day-02/quoteSwap/quoteSwap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quoteSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is to fix it by swapping the right words with code
+            // Create a method called QuoteSwap()
+            // Also, print the sentence to the output with spaces in between.
+            Console.WriteLine(QuoteSwap(list));
+            // Expected output: "What I cannot create I do not understand." 
+            Console.ReadLine();
+        }
+        static string QuoteSwap(List<string> list)
+        {
+            int index = list.IndexOf("cannot");
+            int index2 = list.IndexOf("do");
+            string tempElement = list[index];
+            list[index] = list[index2];
+            list[index2] = tempElement;
+
+            return string.Join(" ", list);
+        }
+    }
+}
