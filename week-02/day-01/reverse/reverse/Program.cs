caa685e5d6d692bdac9d0d88ae73618d2e7ea6e4
Nora Giczy <zynori@gmail.com>
2018-05-21 17:49:24 +0200

    refactor(Reversed)

--- week-02/day-01/reverse/reverse/Program.cs
+++ week-02/day-01/reverse/reverse/Program.cs
@@ -10,18 +10,31 @@ namespace reverse
     {
         static void Main(string[] args)
         {
-            int[] aj = new int[] { 3, 4, 5, 6, 7 };
-            int[] temp = new int[aj.Length];
-            for (int i = 0; i < aj.Length; i++)
-            {
-                temp[i] = aj[aj.Length - i - 1];
-                Console.WriteLine(temp[i]);
-            }
-            Console.ReadLine();
             // - Create an array variable named `aj`
             //   with the following content: `[3, 4, 5, 6, 7]`
             // - Reverse the order of the elements in `aj`
             // - Print the elements of the reversed `aj`
+            int[] aj = { 3, 4, 5, 6, 7 };
+            aj = Reversed(aj);
+
+            for (int i = 0; i < aj.Length; i++)
+            {
+                Console.WriteLine(aj[i]);
+            }
+
+            Console.ReadLine();
+        }
+
+        static int[] Reversed(int[] inputElements)
+        {
+            int[] result = new int [inputElements.Length];
+            
+            for (int i = 0; i < inputElements.Length; i++)
+            {
+                result[i] = inputElements[inputElements.Length-i-1];
+            }
+
+            return result;
         }
     }
 }
