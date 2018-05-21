2abfc78c03873ae636357818e99e02317960865a
Nora Giczy <zynori@gmail.com>
2018-05-21 17:11:41 +0200

    typo(Unique)

--- week-02/day-01/unique/unique/Program.cs
+++ week-02/day-01/unique/unique/Program.cs
@@ -11,31 +11,34 @@ namespace unique
     {
         static void Main(string[] args)
         {
-            List<int> list = Unique(new int[] { 1, 11, 34, 11, 52, 61, 1, 34 });
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.Write(list[i] + " ");
-            }
-            Console.ReadLine();
             //  Create a function that takes a list of numbers as a parameter
             //  Returns a list of numbers where every number in the list occurs only once
 
             //  Example
             // Console.WriteLine(Unique({ 1, 11, 34, 11, 52, 61, 1, 34}));
             //  should print: `[1, 11, 34, 52, 61]`
+            List<int> inputList = new List<int> { 1, 11, 34, 11, 52, 61, 1, 34 };
+            int[] resultList = Unique(inputList).ToArray();
+            for (int i = 0; i < resultList.Length; i++)
+            {
+                Console.WriteLine(resultList[i]);
+            }
+
+            Console.ReadLine();
         }
 
-        public static List<int> Unique(int[] array)
+        static List<int> Unique(List<int> inputList)
         {
-            List<int> list = new List<int>();
-            for (int i = 0; i < array.Length; i++)
+            List<int> resultList = new List<int>();
+
+            for (int i = 0; i < inputList.Count; i++)
             {
-                if (!list.Contains(array[i]))
+                if (!resultList.Contains(inputList[i]))
                 {
-                    list.Add(array[i]);
+                    resultList.Add(inputList[i]);
                 }
             }
-            return list;
+            return resultList;
         }
     }
 }
