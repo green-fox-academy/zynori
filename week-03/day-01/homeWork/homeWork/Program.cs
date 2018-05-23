c398a02a0266c6cad3671f0435e47f0b966ec28d
Nora Giczy <zynori@gmail.com>
2018-05-23 11:52:05 +0200

    feat(postIt):Done

---
+++ week-03/day-01/homeWork/homeWork/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tree1Type = "";
+            string tree1LeafColor = "";
+            int tree1Age = 0;
+            string tree1Sex = "";
+
+            string tree2Type = "";
+            string tree2LeafColor = "";
+            int tree2Age = 0;
+            string tree2Sex = "";
+
+            string tree3Type = "";
+            string tree3LeafColor = "";
+            int tree3Age = 0;
+            string tree3Sex = "";
+
+            string tree4Type = "";
+            string tree4LeafColor = "";
+            int tree4Age = 0;
+            string tree4Sex = "";
+
+            string tree5Type = "";
+            string tree5LeafColor = "";
+            int tree5Age = 0;
+            string tree5Sex = "";
+        }
+    }
+}
---
+++ week-03/day-02/postIt/postIt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt postItFirst = new PostIt("orange", "Idea 1", "blue");
+            PostIt postItSecond = new PostIt("pink", "Awesome", "black");
+            PostIt postItThird = new PostIt("yellow", "Superb!", "green");
+        }
+    }
+
+    class PostIt
+    {
+        public string backgroundColor;
+        public string text;
+        public string textColor;
+   
+        public PostIt(string backgroundColor, string text, string textColor)
+        {
+            this.backgroundColor = backgroundColor;
+            this.text = text;
+            this.textColor = textColor;
+        }
+    }
+}
