eb924d4b1fb09112a264c1c5887d0eac111a688d
Nora Giczy <zynori@gmail.com>
2018-05-23 14:35:21 +0200

    add (Animals)

---
+++ week-03/day-02/animal/animal/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal horse = new Animal(50, 50);
+            horse.Eat();
+            horse.Drink();
+            horse.Play();
+            Console.WriteLine($"Thirst level of your horse: {horse.numberOfThirst}");
+            Console.WriteLine($"Hunger level of your horse: {horse.numberOfHunger}");
+            Console.ReadLine();
+        }
+    }
+
+    class Animal
+    {
+        public int numberOfHunger;
+        public int numberOfThirst;
+
+        public Animal(int numberOfHunger, int numberOfThirst)
+        {
+            this.numberOfHunger = numberOfHunger;
+            this.numberOfThirst = numberOfThirst;
+        }
+
+        public void Eat()
+        {
+            numberOfHunger--;
+        }
+
+        public void Drink()
+        {
+            numberOfThirst--;
+        }
+
+        public void Play()
+        {
+            numberOfHunger++;
+            numberOfThirst++;
+        }
+    }
+}
