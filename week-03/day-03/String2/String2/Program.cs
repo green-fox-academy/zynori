07362381b8af0a0fb7e45fdeb47befc811636fac
Nora Giczy <zynori@gmail.com>
2018-05-24 19:00:59 +0200

    chore(String2)

---
+++ week-03/day-03/String2/String2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = Console.ReadLine();
+            Console.WriteLine(RemovingChars(inputWord));
+            Console.ReadLine();
+        }
+
+        public static string RemovingChars(string inputWord)
+        {
+            string removedWord = inputWord;
+
+            if (removedWord.Length == 0)
+            {
+                return removedWord;
+            }
+            else if (removedWord.Contains("x"))
+            {
+                return removedWord = RemovingChars(inputWord.Replace("x", ""));
+            }
+            else
+            {
+                return removedWord;
+            }
+        }
+    }
+}
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
