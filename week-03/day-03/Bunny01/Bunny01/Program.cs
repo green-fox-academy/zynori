6880cbeeaa0af0e1330802d1db970db0edbc384f
Nora Giczy <zynori@gmail.com>
2018-05-24 16:16:35 +0200

    chore(Bunny02)

---
+++ week-03/day-03/Bunny01/Bunny01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bunny = int.Parse(Console.ReadLine());
+            Console.WriteLine(Recursion(bunny));
+            Console.ReadLine();
+        }
+
+        public static int Recursion(int bunny)
+        {
+            if (bunny == 0)
+            {
+                return bunny;
+            }
+            else
+            {
+                return Recursion(bunny - 1) + 2;
+            }
+        }
+    }
+}
+// We have a number of bunnies and each bunny has two big floppy ears.
+// We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
---
+++ week-03/day-03/Bunny02/Bunny02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Recursion(3));
+            Console.ReadLine();
+        }
+
+        public static int Recursion(int bunny)
+        {
+            if (bunny == 1)
+            {
+                return 2;
+            }
+            else if (bunny % 2 != 0)
+            {
+                return 2 + Recursion(bunny - 1);
+            }
+            else
+            {
+                return 3 + Recursion(bunny - 1); 
+            }
+        }
+    }
+}
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
