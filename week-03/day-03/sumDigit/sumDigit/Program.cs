a222a21377007676d858fbdc6968f4c9481e5f7b
Nora Giczy <zynori@gmail.com>
2018-05-24 11:47:30 +0200

    chore(sumDigit)

---
+++ week-03/day-03/sumDigit/sumDigit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Recursion(50));
+            Console.ReadLine();
+        }
+
+        public static int Recursion(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return n % 10 + Recursion(n / 10);
+            }
+        }
+    }
+}
+// Given a non-negative int n, return the sum of its digits recursively (no loops). 
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
