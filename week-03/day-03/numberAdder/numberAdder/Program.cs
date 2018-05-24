569e14fc440cfa3dc5ac3544f4eaa1b293691658
Nora Giczy <zynori@gmail.com>
2018-05-24 11:04:26 +0200

    chore(numberAdder)

---
+++ week-03/day-03/numberAdder/numberAdder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            Console.WriteLine(Recursion(n));
+            Console.ReadLine();
+        }
+
+        public static int Recursion(int n)
+        {
+            if (n == 100)
+            {
+                return 100;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                return Recursion(n+1);
+            }
+        }
+    }
+}
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
