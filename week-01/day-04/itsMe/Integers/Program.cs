5b458d6096082c739105feecbf348e006af433e7
Nora Giczy <zynori@gmail.com>
2018-05-26 13:27:30 +0200

    typo(ItsMe)

--- week-01/day-04/itsMe/Integers/Program.cs
+++ week-01/day-04/itsMe/Integers/Program.cs
@@ -17,8 +17,17 @@ namespace Integers
             Console.WriteLine("my name: " + myName);
             Console.WriteLine("my age: " + myAge);
             Console.WriteLine("my height: " + myHeight);
-
             Console.ReadLine();
         }
     }
 }
+// Write a program that prints a few details to the terminal window about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
