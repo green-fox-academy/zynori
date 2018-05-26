13e0f5a33ce4c2278544455a30788c60481494a9
Nora Giczy <zynori@gmail.com>
2018-05-26 16:58:46 +0200

    style(drawTriangle)

--- week-01/day-04/drawTriangle/drawTriangle/Program.cs
+++ week-01/day-04/drawTriangle/drawTriangle/Program.cs
@@ -28,3 +28,10 @@ namespace drawTriangle
         }
     }
 }
+// Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+// *
+// **
+// ***
+// ****
+// The triangle should have as many lines as the number was
