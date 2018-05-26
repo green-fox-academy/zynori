47830953eca616add36806426ecc2596a3ed5f1c
Nora Giczy <zynori@gmail.com>
2018-05-26 15:01:43 +0200

    chore(PrintBigger)

---
+++ week-01/day-04/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a numero: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Give me another one: ");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b > a)
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine("They are equals");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+// Write a program that asks for two numbers and prints the bigger one.
