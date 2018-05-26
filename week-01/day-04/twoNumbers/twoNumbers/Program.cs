fc1f4e0495a3ba6c151e8b79f64e7c652755db6f
Nora Giczy <zynori@gmail.com>
2018-05-26 13:28:52 +0200

    typo(TwoNumbers)

--- week-01/day-04/twoNumbers/twoNumbers/Program.cs
+++ week-01/day-04/twoNumbers/twoNumbers/Program.cs
@@ -22,8 +22,14 @@ namespace twoNumbers
             Console.WriteLine(number1 / number2);
             Console.WriteLine(doubleNumber1 / doubleNumber2);
             Console.WriteLine(doubleNumber1 % doubleNumber2);
-
             Console.ReadLine();
         }
     }
 }
+// Create a program that prints a few operations on two numbers: 22 and 13
+// Print the result of 13 added to 22
+// Print the result of 13 substracted from 22
+// Print the result of 22 multiplied by 13
+// Print the result of 22 divided by 13 (as a decimal fraction)
+// Print the integer part of 22 divided by 13
+// Print the reminder of 22 divided by 13
