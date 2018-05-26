ca2ac85d4fc6e13046a5e26b063fe2bf2c163ae7
Nora Giczy <zynori@gmail.com>
2018-05-26 14:42:57 +0200

    refactor(helloUser)

--- week-01/day-04/helloUser/helloUser/Program.cs
+++ week-01/day-04/helloUser/helloUser/Program.cs
@@ -15,8 +15,10 @@ namespace helloUser
             Console.WriteLine("What's your name?");
             userName = Console.ReadLine();
 
-            Console.WriteLine("Hello {0}", userName + "!");
+            Console.WriteLine($"Hello {userName}!");
             Console.ReadLine();
         }
     }
 }
+// Modify this program to greet user instead of the World!
+// The program should ask for the name of the user
