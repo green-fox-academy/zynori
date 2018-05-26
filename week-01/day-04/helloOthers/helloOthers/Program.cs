b4c815f59ac56590e385b24a741ce10a21832972
Nora Giczy <zynori@gmail.com>
2018-05-26 13:23:55 +0200

    typo(HelloOthers)

--- week-01/day-04/helloOthers/helloOthers/Program.cs
+++ week-01/day-04/helloOthers/helloOthers/Program.cs
@@ -13,8 +13,13 @@ namespace helloOthers
             Console.WriteLine("Hello, Jenő");
             Console.WriteLine("Hello, Béla");
             Console.WriteLine("Hello, István");
-            Console.ReadLine();
-            
+            Console.ReadLine(); 
         }
     }
 }
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
