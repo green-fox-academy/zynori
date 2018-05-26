6364a90fef746c20148ce1e606178d3296d925c2
Nora Giczy <zynori@gmail.com>
2018-05-26 14:56:21 +0200

    style(oneTwoALot)

--- week-01/day-04/oneTwoALot/oneTwoALot/Program.cs
+++ week-01/day-04/oneTwoALot/oneTwoALot/Program.cs
@@ -34,3 +34,8 @@ namespace oneTwoALot
         }
     }
 }
+// Write a program that reads a number form the standard input,
+// If the number is zero or smaller it should print: Not enough
+// If the number is one it should print: One
+// If the number is two it should print: Two
+// If the number is more than two it should print: A lot
