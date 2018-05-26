d8f6153344fcf2dd82195130468a22802ff3fa00
Nora Giczy <zynori@gmail.com>
2018-05-26 13:41:44 +0200

    chore(Bmi)

---
+++ week-01/day-04/Bmi/Bmi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+            double bmiResult = massInKg / (heightInM * heightInM);
+
+            Console.WriteLine(bmiResult);
+            Console.ReadLine();
+        }
+    }
+}
+// Print the Body mass index (BMI) based on these values
