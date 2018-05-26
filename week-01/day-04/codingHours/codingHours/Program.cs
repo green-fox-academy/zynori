2208a555ab298bb889aec1c423c61beb59185d9c
Nora Giczy <zynori@gmail.com>
2018-05-26 13:30:04 +0200

    typo(CodingHours)

--- week-01/day-04/codingHours/codingHours/Program.cs
+++ week-01/day-04/codingHours/codingHours/Program.cs
@@ -18,12 +18,14 @@ namespace codingHours
 
             Console.WriteLine(finalCodingHours1);
             Console.WriteLine(finalCodingHours2);
-
             Console.WriteLine((finalCodingHours1/finalCodingHours2) * 100);
-
-
             Console.ReadLine();
-            
         }
     }
 }
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
