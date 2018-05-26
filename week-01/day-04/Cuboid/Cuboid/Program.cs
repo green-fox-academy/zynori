e840435c040f04e5904008e36f122c5d789f2f9d
Nora Giczy <zynori@gmail.com>
2018-05-26 14:37:23 +0200

    typo(Cuboid)

--- week-01/day-04/Cuboid/Cuboid/Program.cs
+++ week-01/day-04/Cuboid/Cuboid/Program.cs
@@ -21,6 +21,5 @@ namespace Cuboid
 }
 // Write a program that stores 3 sides of a cuboid as variables (doubles)
 // The program should write the surface area and volume of the cuboid like:
-//
 // Surface Area: 600
 // Volume: 1000
