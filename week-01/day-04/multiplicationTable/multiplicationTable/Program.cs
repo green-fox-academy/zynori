2d23a4051be48a24a76383995025e39c3d8e943e
Nora Giczy <zynori@gmail.com>
2018-05-26 16:54:06 +0200

    style(multiplicationTabl)

--- week-01/day-04/multiplicationTable/multiplicationTable/Program.cs
+++ week-01/day-04/multiplicationTable/multiplicationTable/Program.cs
@@ -10,7 +10,7 @@ namespace multiplicationTable
     {
         static void Main(string[] args)
         {
-            Console.Write("give me a number: ");
+            Console.Write("Give me a number: ");
             int number = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < 1000; i++)
@@ -22,3 +22,16 @@ namespace multiplicationTable
         }
     }
 }
+// Create a program that asks for a number and prints the multiplication table with that number
+// Example:
+// The number 15 should print:
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
