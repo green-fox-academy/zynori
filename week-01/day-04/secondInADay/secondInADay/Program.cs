c9f0149ec2590c3dc4a6e125b987243e05f538e4
Nora Giczy <zynori@gmail.com>
2018-05-26 14:39:16 +0200

    typo(secondInADay)

--- week-01/day-04/secondInADay/secondInADay/Program.cs
+++ week-01/day-04/secondInADay/secondInADay/Program.cs
@@ -13,15 +13,14 @@ namespace secondInADay
             int currentHours = 14;
             int currentMinutes = 34;
             int currentSeconds = 42;
-
             int seconds = currentSeconds + currentMinutes * 60 + currentHours * 60 * 60;
             int secondsInADay = 24 * 60 * 60;
-
             int remainingSec = secondsInADay - seconds;
 
             Console.WriteLine(remainingSec);
-
             Console.ReadLine();
         }
     }
 }
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by the variables
