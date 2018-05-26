cf1e2c1a70e9a66327b15aee39fb926aaebcdc78
Nora Giczy <zynori@gmail.com>
2018-05-26 14:55:17 +0200

    style(oddEven)

--- week-01/day-04/oddEven/oddEven/Program.cs
+++ week-01/day-04/oddEven/oddEven/Program.cs
@@ -13,15 +13,18 @@ namespace oddEven
             Console.WriteLine("give me a number:");
             int number = int.Parse(Console.ReadLine());
 
-                if (number / 2 == 0)
-                {
-                    Console.WriteLine("odd");
-                }
-                else
-                {
-                    Console.WriteLine("even");
-                }
-                int.Parse(Console.ReadLine());
+            if (number / 2 == 0)
+            {
+                Console.WriteLine("odd");
+            }
+            else
+            {
+                Console.WriteLine("even");
+            }
+
+            int.Parse(Console.ReadLine());
         }
     }
 }
+// Write a program that reads a number from the standard input,
+// Then prints "Odd" if the number is odd, or "Even" if it is even.
