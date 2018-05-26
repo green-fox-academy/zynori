88a4ffbdcb26f8072778ba4d1c7bd5feafcb3ed9
Nora Giczy <zynori@gmail.com>
2018-05-26 14:36:53 +0200

    chore(Cuboid)

---
+++ week-01/day-04/Cuboid/Cuboid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 12;
+            double surfaceArea = 6 * (a * a);
+            double volume = a * a * a;
+
+            Console.Write("Surface Area: " + surfaceArea + "\n" + "Volume: " + volume);
+            Console.ReadLine();
+        }
+    }
+}
+// Write a program that stores 3 sides of a cuboid as variables (doubles)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
