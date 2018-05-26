093c5813fcbf83e3e88c52e5bb098499b348ddc4
Nora Giczy <zynori@gmail.com>
2018-05-26 17:18:16 +0200

    drawPyramid

---
+++ week-01/day-04/drawPyramid/drawPyramid/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drawPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me one numeroooo: ");
+            int pyramidNum = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= pyramidNum; i++)
+            {
+                for (int j = 1; j < pyramidNum - i + 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+// Write a program that reads a number from the standard input, then draws a
+// pyramid like this:
+//    *
+//   ***
+//  *****
+// *******
+// The pyramid should have as many lines as the number was
