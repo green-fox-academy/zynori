520d714411c61a8e687e722a56644c5d1c112e22
Nora Giczy <zynori@gmail.com>
2018-05-27 13:33:44 +0200

    chore(IsInList)

---
+++ week-02/day-02/IsInList/IsInList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsInList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 2, 4, 6, 8, 10, 14, 16 };
+
+            Console.WriteLine(CheckNums(list));
+            Console.ReadLine();
+        }
+
+        public static bool CheckNums(List<int> list)
+        {
+            bool contains = list.Contains(4) && list.Contains(8) && list.Contains(12) && list.Contains(16);
+            return contains;
+        }
+    }
+}
+// Check if list contains all of the following elements: 4,8,12,16
+// Create a method that accepts list as an input
+// it should return "true" if it contains all, otherwise "false"
