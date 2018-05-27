712cae5c437ea58955439382d5978a11e1d7a1c7
Nora Giczy <zynori@gmail.com>
2018-05-27 11:27:09 +0200

    style(takesLonger)

--- week-02/day-02/takesLonger/takesLonger/Program.cs
+++ week-02/day-02/takesLonger/takesLonger/Program.cs
@@ -11,17 +11,16 @@ namespace takesLonger
         static void Main(string[] args)
         {
             string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
-
             int index = quote.IndexOf("It ");
             string indexQuote = quote.Insert(index + "It ".Length, "always takes longer than ");
-            quote = indexQuote;
 
-            // When saving this quote a disk error has occured. Please fix it.
-            // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
-            // Using pieces of the quote variable (instead of just redefining the string)
+            quote = indexQuote;
 
             Console.WriteLine(quote);
             Console.ReadLine();
         }
     }
 }
+// When saving this quote a disk error has occured. Please fix it.
+// Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+// Using pieces of the quote variable (instead of just redefining the string)
