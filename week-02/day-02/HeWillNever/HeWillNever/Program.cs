da19d48131cc90aef265ee78c5ee7dbce86f8874
Nora Giczy <zynori@gmail.com>
2018-05-27 13:59:19 +0200

    chore(HeWillNever)

---
+++ week-02/day-02/HeWillNever/HeWillNever/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeWillNever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            int[] notSoCrypticMessage = { 1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11 };
+
+            var map = new Dictionary<int, string>();
+
+            map.Add(7, "run around and desert you");
+            map.Add(50, "tell a lie and hurt you ");
+            map.Add(49, "make you cry, ");
+            map.Add(2, "let you down");
+            map.Add(12, "give you up, ");
+            map.Add(1, "Never gonna ");
+            map.Add(11, "\n");
+            map.Add(3, "say goodbye ");
+
+            foreach (var item in notSoCrypticMessage)
+            {
+                output += map[item];
+            }
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
+// Things are a little bit messed up
+// Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+// Assemble the fragments into the out variable
