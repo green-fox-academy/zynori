f3a9183843c5764ac6574895fc74229bcd13f1cc
Nora Giczy <zynori@gmail.com>
2018-05-27 14:04:49 +0200

    style(divideByZero)

--- week-02/day-03/divideByZero/divideByZero/Program.cs
+++ week-02/day-03/divideByZero/divideByZero/Program.cs
@@ -13,13 +13,10 @@ namespace divideByZero
         {
             Console.WriteLine("Give me a number: ");
             int givenNumber = Int32.Parse(Console.ReadLine());
-            Console.WriteLine(DoTheMath(givenNumber));
 
+            Console.WriteLine(DoTheMath(givenNumber));
             Console.ReadLine();
-            // Create a function that takes a number
-            // divides ten with it,
-            // and prints the result.
-            // It should print "fail" if the parameter is 0
+            
         }
 
         static string DoTheMath(int givenNumber)
@@ -38,3 +35,7 @@ namespace divideByZero
         }
     }
 }
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print "fail" if the parameter is 0
