f0a6e1d4e7f27f533ed76256d56cc438fc9058c1
Nora Giczy <zynori@gmail.com>
2018-05-27 14:06:03 +0200

    style(printEachLine)

--- week-02/day-03/printEachLine/printEachLine/Program.cs
+++ week-02/day-03/printEachLine/printEachLine/Program.cs
@@ -31,12 +31,11 @@ namespace printEachLine
                 Console.WriteLine("Unable to read file: my-file.txt"); ;
             }
 
-            Console.ReadLine();
-
-            // Write a program that opens a file called "my-file.txt", then prints
-            // each of lines form the file.
-            // If the program is unable to read the file (for example it does not exists),
-            // then it should print an error message like: "Unable to read file: my-file.txt"
+            Console.ReadLine();   
         }
     }
 }
+// Write a program that opens a file called "my-file.txt", then prints
+// each of lines form the file.
+// If the program is unable to read the file (for example it does not exists),
+// then it should print an error message like: "Unable to read file: my-file.txt"
