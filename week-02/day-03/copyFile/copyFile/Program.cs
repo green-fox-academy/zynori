9426b7640edfaee86766222ea516e6d8c8d161e9
Nora Giczy <zynori@gmail.com>
2018-05-27 14:09:30 +0200

    style(copyFile)

--- week-02/day-03/copyFile/copyFile/Program.cs
+++ week-02/day-03/copyFile/copyFile/Program.cs
@@ -29,11 +29,10 @@ namespace copyFile
                 }
             }
             
-            Console.ReadLine();
-            
-            // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
-            // It should take the filenames as parameters
-            // It should return a boolean that shows if the copy was successful
+            Console.ReadLine();   
         }
     }
 }
+// Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
