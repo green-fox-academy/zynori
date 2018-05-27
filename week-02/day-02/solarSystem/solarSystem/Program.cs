a25148603f43fcb17541db49269def6a8f31a7ca
Nora Giczy <zynori@gmail.com>
2018-05-27 11:30:36 +0200

    style(solarSystem)

--- week-02/day-02/solarSystem/solarSystem/Program.cs
+++ week-02/day-02/solarSystem/solarSystem/Program.cs
@@ -13,12 +13,7 @@ namespace solarSystem
             var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
             planetList.Insert(4, "Saturn");
 
-            // Saturn is missing from the planetList
-            // Insert it into the correct position
-            // Create a method called PutSaturn() which has list parameter and returns the correct list of planets as a string.
-
             Console.WriteLine(PutSaturn(planetList));
-            // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
             Console.ReadLine();
         }
 
@@ -28,8 +23,7 @@ namespace solarSystem
 
             foreach (string currentPlanet in inputPlanet)
             {
-                planetBuilder
-                .Append(currentPlanet);
+                planetBuilder.Append(currentPlanet);
 
                 if (inputPlanet.IndexOf(currentPlanet) != inputPlanet.Count - 1)
                 {
@@ -41,3 +35,7 @@ namespace solarSystem
         }
     }
 }
+// Saturn is missing from the planetList
+// Insert it into the correct position
+// Create a method called PutSaturn() which has list parameter and returns the correct list of planets as a string.
+// Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
