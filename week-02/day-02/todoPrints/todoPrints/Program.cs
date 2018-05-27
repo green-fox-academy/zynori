2c95c87f1422eba8d890ac71bc6152bd3075f1d1
Nora Giczy <zynori@gmail.com>
2018-05-27 11:27:55 +0200

    style(todoPrints)

--- week-02/day-02/todoPrints/todoPrints/Program.cs
+++ week-02/day-02/todoPrints/todoPrints/Program.cs
@@ -11,25 +11,21 @@ namespace todoPrints
         static void Main(string[] args)
         {
             string todoText = " - Buy milk\n";
-
             int games = todoText.IndexOf("milk\n");
             string indexGames = todoText.Insert(games + "milk\n".Length, " - Download games\n" + "\t- Diablo");
 
             todoText = indexGames;
-            
-            // Add "My todo:" to the beginning of the todoText
-            // Add " - Download games" to the end of the todoText
-            // Add " - Diablo" to the end of the todoText but with indentation
-
-            // Expected output:
-
-            // My todo:
-            //  - Buy milk
-            //  - Download games
-            //      - Diablo
 
             Console.WriteLine("My todo:\n" + todoText);
             Console.ReadLine();
         }
     }
 }
+// Add "My todo:" to the beginning of the todoText
+// Add " - Download games" to the end of the todoText
+// Add " - Diablo" to the end of the todoText but with indentation
+// Expected output:
+// My todo:
+//  - Buy milk
+//  - Download games
+//      - Diablo
