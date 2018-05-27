07709cec5cc866e187a4f8f38ec6b929fecccff0
Nora Giczy <zynori@gmail.com>
2018-05-27 11:09:51 +0200

    style(diagonalMatrix)

--- week-02/day-01/diagonalMatrix/diagonalMatrix/Program.cs
+++ week-02/day-01/diagonalMatrix/diagonalMatrix/Program.cs
@@ -30,12 +30,10 @@ namespace diagonalMatrix
             Console.ReadLine();
             // - Create (dynamically) a two dimensional array
             //   with the following matrix. Use a loop!
-            //
             //   1 0 0 0
             //   0 1 0 0
             //   0 0 1 0
             //   0 0 0 1
-            //
             // - Print this two dimensional array to the output
         }
     }
