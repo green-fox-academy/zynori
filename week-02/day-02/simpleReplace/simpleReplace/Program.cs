a472e7a2f5e8cdecb096bc6bda8a43bdf2cbb763
Nora Giczy <zynori@gmail.com>
2018-05-27 11:24:40 +0200

    style(simpleReplace)

--- week-02/day-02/simpleReplace/simpleReplace/Program.cs
+++ week-02/day-02/simpleReplace/simpleReplace/Program.cs
@@ -12,14 +12,13 @@ namespace simpleReplace
         {
             string example = "In a dishwasher far far away";
 
-            example = example.Replace("dishwasher", "galaxy");
-        
-            // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
-            // Please fix it for me!
-            // Expected ouput: In a galaxy far far away
+            example = example.Replace("dishwasher", "galaxy");           
 
             Console.WriteLine(example);
             Console.ReadLine();
         }
     }
 }
+// I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
