7fbd9d99d55e3ca6a0a514934f74601e34be2625
Nora Giczy <zynori@gmail.com>
2018-05-30 12:32:13 +0200

    feat(Cloneable): implement cloneable interface

--- week-04/Day-03/Cloneable/Cloneable/Program.cs
+++ week-04/Day-03/Cloneable/Cloneable/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HomeWork;
 
 namespace Cloneable
 {
@@ -10,6 +11,7 @@ namespace Cloneable
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
---
+++ week-04/Day-03/Cloneable/Cloneable/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Student : Person, ICloneable
+    {
+        private string previousOrganization;
+        private double skippedDays;
+
+        public Student(string name, int age, string gender, string previousOrganization) : base(name, age, gender)
+        {
+            this.previousOrganization = previousOrganization;
+            skippedDays = 0;
+        }
+        
+        public Student() : base()
+        {
+            previousOrganization = "The School of Life";
+            skippedDays = 0;
+        }
+        
+        public override void GetGoal()
+        { 
+            Console.WriteLine("Be a junior software developer.");
+        }
+
+        public override void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, a {Age} year old {Gender} from {this.previousOrganization} who skipped {this.skippedDays} days from the course already.");
+        }
+
+        public void SkipDays(int numberOfDays)
+        {
+            skippedDays += numberOfDays;
+        }
+
+        object ICloneable.Clone()
+        {
+            Student clonedStudent = new Student(Name, Age, Gender, previousOrganization);
+            return clonedStudent;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age} {Gender} {previousOrganization}";
+        }
+    }
+}
