344676ec419d0ee5d1c4e09261c9e5effa3c442a
Nora Giczy <zynori@gmail.com>
2018-05-31 15:56:57 +0200

    feat(Sum) finish

---
+++ week-04/Day-04/Sum/Sum/Sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    public class Sums
+    {
+        public static int Summing()
+        {
+            List<int> sumList = new List<int>() { 2, 4 };
+
+            return sumList.Sum();
+        }
+    }
+}
---
+++ week-04/Day-04/Sum/TestEnviroment/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using Sum;
+
+namespace TestEnviroment
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase]
+        public void Should()
+        {
+            Assert.AreEqual(Sums.Summing(), 6 );
+        }
+    }
+}
