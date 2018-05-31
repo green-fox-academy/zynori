ea3c79a205f114bd9ca58140e1cf09457ed4f55c
Nora Giczy <zynori@gmail.com>
2018-05-31 12:39:21 +0200

    chore(Apples)

---
+++ week-04/Day-04/Apples/TestEnviroment/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace TestEnviroment
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase]
+        public void WorksOrNot()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
