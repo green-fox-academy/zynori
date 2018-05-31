bd7dc791038c2d195c996d246005c2a0631eae2a
Nora Giczy <zynori@gmail.com>
2018-05-31 16:41:57 +0200

    chore(Anagram)

---
+++ week-04/Day-04/Anagram/Anagram/InputString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class InputString
+    {
+        public bool IsItAnagram(string first, string second)
+        {
+            char[] result = second.ToCharArray();
+            Array.Reverse(result);
+            second = new string(result);
+
+            return (first.ToLower() == second.ToLower());
+        }
+    }
+}
---
+++ week-04/Day-04/Anagram/TestEnviroment/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using Anagram;
+
+namespace TestEnviroment
+{
+    [TestFixture]
+    public class Test
+    {
+        InputString anagram;
+
+        [SetUp]
+        public void SetUp()
+        {
+            anagram = new InputString();
+        }
+
+        [TestCase("Abba", "Abba")]
+        public void IsItAnagram(string first, string second)
+        {
+            Assert.AreEqual(true, anagram.IsItAnagram(first, second));
+        }
+
+        [TestCase("Aleb", "Bela")]
+        public void IsItReverse(string first, string second)
+        {
+            Assert.AreEqual(true, anagram.IsItAnagram(first, second));
+        }
+
+        [TestCase("", "")]
+        public void IsItEmpty(string first, string second)
+        {
+            Assert.AreEqual(true, anagram.IsItAnagram(first, second));
+        }
+
+        [TestCase(null, null)]
+        public void IsItNull(string first, string second)
+        {
+            Assert.Throws<NullReferenceException>(() => anagram.IsItAnagram(first, second));
+        }
+    }
+}
