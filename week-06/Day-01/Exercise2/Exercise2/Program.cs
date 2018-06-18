39fb7a653d1089a3827fbd9892b5d481b00d59ac
Nora Giczy <zynori@gmail.com>
2018-06-18 11:28:32 +0200

    chore(exercise2)

---
+++ week-06/Day-01/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            
+            double avgOddNums =
+                (from num in n
+                where num % 2 == 1
+                select num).Average();
+
+            Console.WriteLine(avgOddNums);
+            Console.ReadLine();
+        }
+    }
+}
