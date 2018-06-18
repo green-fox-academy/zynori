40062bce6c4e3ce6fdd94552236dffb9bc4bc5e1
Nora Giczy <zynori@gmail.com>
2018-06-18 14:14:04 +0200

    chore(Exercise5)

---
+++ week-06/Day-01/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var freq =
+                from x in n
+                group x by x into y
+                select y;
+
+            foreach (var item in freq)
+            {
+                Console.WriteLine(item.Key + " appears: " + item.Count() + " times");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
+//Write a LINQ Expression to find the frequency of numbers in the following array:
