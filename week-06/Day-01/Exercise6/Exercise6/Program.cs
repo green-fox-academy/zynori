03fcdf94f17df8d7de9a5b4c8d8181c5a6e332ff
Nora Giczy <zynori@gmail.com>
2018-06-18 14:39:17 +0200

    chore(Exercise6)

---
+++ week-06/Day-01/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write something pls");
+            string inputStuff = Console.ReadLine();
+
+            var freq =
+                from a in inputStuff
+                group a by a into b
+                select b;
+
+            freq.ToList().ForEach(b => Console.WriteLine(b.Key + " appears: " + b.Count() + " times"));
+
+            Console.ReadLine();
+        }
+    }
+}
