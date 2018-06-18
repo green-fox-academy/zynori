8ada91441383e9dccc9226309b304d74964c06a7
Nora Giczy <zynori@gmail.com>
2018-06-18 12:19:25 +0200

    chore(Exercise4)

---
+++ week-06/Day-01/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> posNum =
+                 from num in n
+                 where num > 0
+                 select num * num;
+
+            foreach (var num in posNum)
+            {
+                Console.WriteLine(num);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-06/Day-01/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            IEnumerable<int> squaredVal =
+                from nums in n
+                where nums * nums > 20
+                select nums;
+
+            squaredVal.ToList().ForEach(nums => Console.WriteLine(nums));
+
+            Console.ReadLine();
+        }
+    }
+}
