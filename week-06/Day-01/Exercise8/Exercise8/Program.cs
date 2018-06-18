8906cd5e198f05733c730f1393128e9eac3e6ea9
Nora Giczy <zynori@gmail.com>
2018-06-18 16:26:11 +0200

    chore(Exercise8)

---
+++ week-06/Day-01/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Helloka bela Ez nagy betu";
+
+            var upperA =
+                from chars in a
+                where chars.Equals(Char.ToUpper(chars))
+                select chars;
+
+            upperA.ToList().ForEach(chars => Console.Write(chars));
+
+            Console.ReadLine();
+        }
+    }
+}
+// Write a LINQ Expression to find the uppercase characters in a string!
