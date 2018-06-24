872c2845c83840aa4990ea6c77c6b2a4af3f8650
Nora Giczy <zynori@gmail.com>
2018-06-24 13:26:05 +0200

    chore(Exercise10): find fox in a list by type or color

---
+++ week-06/Day-01/Exercise10/Exercise10/Fox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Fox
+    {
+        private string name;
+        private string type;
+        private string color;
+        /*
+        public Fox(string name, string type, string color)
+        {
+            this.name = name;
+            this.type = type;
+            this.color = color;
+        }
+        */
+        public string Name { get => name; set => name = value; }
+        public string Type { get => type; set => type = value; }
+        public string Color { get => color; set => color = value; }
+    }
+}
---
+++ week-06/Day-01/Exercise10/Exercise10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fox> foxes = new List<Fox>()
+            {
+            new Fox() {Name = "Bela", Type = "arctic", Color = "white" },
+            new Fox() {Name = "Jeno", Type = "pallida", Color = "green" },
+            new Fox() {Name = "Klari", Type = "corsac", Color = "yellow" },
+            new Fox() {Name = "Sandor", Type = "pallida", Color = "green" },
+            new Fox() {Name = "Pista", Type = "ocelot", Color = "green" }
+            };
+
+            IEnumerable<Fox> listOfgreenFoxes = from fox in foxes
+                                     where fox.Color.Equals("green")
+                                     select fox;
+
+            foreach (Fox gfox in listOfgreenFoxes)
+            {
+                Console.WriteLine("This is green fox: " + gfox.Name);
+            }
+
+            IEnumerable<Fox> listOfGreenPallida = from fox in foxes
+                                                  where fox.Color.Equals("green") && fox.Type.Equals("pallida")
+                                                  select fox;
+
+            foreach (Fox gpFox in listOfGreenPallida)
+            {
+                Console.WriteLine("This is green fox and pallida: " + gpFox.Name);
+            }
+
+            IEnumerable<Fox> findGreenFox = foxes.Where(f => f.Color.Equals("green"));
+            findGreenFox.ToList().ForEach(f => Console.WriteLine("This is green fox: " + f.Name));
+
+            Console.ReadLine();
+        }
+    }
+}
+//Create a Fox class with 3 properties(name, type, color) Fill a list with at least 5 foxes, 
+//it's up to you how you name/create them! Write a LINQ Expression to find the foxes with green color! 
+//Write a LINQ Expression to find the foxes with green color and pallida type!
