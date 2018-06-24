58f69f44bdbd3ffd03d14f696f157db29dc63334
Nora Giczy <zynori@gmail.com>
2018-06-24 12:27:53 +0200

    chore(exercise9): char array to string

---
+++ week-06/Day-01/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = new char[5];
+            chars[0] = 'H';
+            chars[1] = 'e';
+            chars[2] = 'l';
+            chars[3] = 'l';
+            chars[4] = 'o';
+
+            string charToString = String.Join("", chars.Select(s => s.ToString()));
+
+            Console.WriteLine(charToString);
+            Console.ReadLine();
+        }
+    }
+}
