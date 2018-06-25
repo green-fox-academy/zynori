7bd5db5bb5f1f4fd989306c82fb688db812c214a
Nora Giczy <zynori@gmail.com>
2018-06-25 14:46:18 +0200

    feat(webapp) count calls

---
+++ week-07/FirstApp/FirstApp/Controllers/RESTController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class RESTController : Controller
+    {
+        [Route("api")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            Greeting greets = new Greeting(name);
+            
+            return new JsonResult(greets);
+        }
+    }
+}
---
+++ week-07/FirstApp/FirstApp/Models/Greeting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Models
+{
+    public class Greeting
+    {
+        public long Id { get; set; }
+        public static long IdCount { get; set; }
+        public string Content { get; set; }
+
+        public Greeting(string name)
+        {
+            Content = name;
+            Id = IdCount++;
+        }
+    }
+}
