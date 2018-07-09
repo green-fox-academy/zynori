111735f1883816e6d7c0c27d5ee17399faf4bd9f
Nora Giczy <zynori@gmail.com>
2018-07-09 14:11:02 +0200

    feat: add greeter

--- week-09/Day-01/Frontend/Frontend/Controllers/HomeController.cs
+++ week-09/Day-01/Frontend/Frontend/Controllers/HomeController.cs
@@ -19,11 +19,43 @@ namespace Frontend.Controllers
         {
             if (input != null)
             {
-                return Json(new {received=input, result=input*2 });
+                return Json(new
+                {
+                    received =input, result=input*2
+                });
             }
             else
             {
-                return Json(new { error = "Please provide an input!" });
+                return Json(new
+                {
+                    error = "Please provide an input!"
+                });
+            }
+        }
+
+        [HttpGet("/greeter")]
+        public IActionResult Greeter([FromQuery] string name, [FromQuery] string title)
+        {
+            if (name == null)
+            {
+                return Json(new
+                {
+                    error = "Please provide a name!"
+                });
+            }
+            else if (title == null)
+            {
+                return Json(new
+                {
+                    error = "Please provide a title!"
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    welcome_message = $"Oh, hi there {name}, my dear {title}!"
+                });
             }
         }
     }
