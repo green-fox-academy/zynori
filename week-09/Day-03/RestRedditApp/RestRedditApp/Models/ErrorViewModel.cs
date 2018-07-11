bfc81bb5fef94d3d84df9796854865b3c7a8946e
Nora Giczy <zynori@gmail.com>
2018-07-11 10:42:18 +0200

    chore(RestRedditApp)

---
+++ week-09/Day-03/RestRedditApp/RestRedditApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestRedditApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ week-09/Day-03/RestRedditApp/RestRedditApp/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestRedditApp.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int Score { get; set; }
+    }
+}
