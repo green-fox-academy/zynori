8895f19206cc622e3eb816bc20c8cd6bdbfd4bf5
Nora Giczy <zynori@gmail.com>
2018-07-11 11:53:58 +0200

    create service

---
+++ week-09/Day-03/RestRedditApp/RestRedditApp/Services/PostService.cs
@@ -0,0 +1,41 @@
+﻿using RestRedditApp.Models;
+using RestRedditApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestRedditApp.Services
+{
+    public class PostService
+    {
+        PostRepository postRepository;
+
+        public PostService(PostRepository postRepository)
+        {
+            this.postRepository = postRepository;
+        }
+
+        public List<Post> GetAllPost()
+        {
+            return postRepository.ReadAllPost();
+        }
+
+        public void AddNewPost(Post post)
+        {
+            postRepository.Create(post);
+        }
+
+        public void UpVote(int id)
+        {
+            postRepository.GetPost(id).Score++;
+            postRepository.Update(postRepository.GetPost(id));
+        }
+
+        public void DownVote(int id)
+        {
+            postRepository.GetPost(id).Score--;
+            postRepository.Update(postRepository.GetPost(id));
+        }
+    }
+}
