7627d05807cfb28fcb0391cde7e59190de4a7223
Nora Giczy <zynori@gmail.com>
2018-07-11 11:14:58 +0200

    feat: add crud

---
+++ week-09/Day-03/RestRedditApp/RestRedditApp/Repositories/PostRepository.cs
@@ -0,0 +1,46 @@
+﻿using RestRedditApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestRedditApp.Repositories
+{
+    public class PostRepository
+    {
+        private PostContext postContext;
+
+        public PostRepository(PostContext postContext)
+        {
+            this.postContext = postContext;
+        }
+
+        public void Create(Post post)
+        {
+            postContext.Add(post);
+            postContext.SaveChanges();
+        }
+
+        public List<Post> ReadAllPost()
+        {
+            return postContext.Posts.ToList();
+        }
+
+        public Post GetPost(int id)
+        {
+            return postContext.Posts.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Update(Post post)
+        {
+            postContext.Update(post);
+            postContext.SaveChanges();
+        }
+
+        public void Delete(Post post)
+        {
+            postContext.Remove(post);
+            postContext.SaveChanges();
+        }
+    }
+}
